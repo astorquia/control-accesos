53813f4fe0414f18c00f6259a711b4ea122a54d7
astorquia <astorquia.antonio@gmail.com>
2019-10-07 18:09:07 +0200

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'control-accesos';
+  private file: string;      
+  public leerFicheroExcel(event) {
+    let reader = new FileReader();
+    if(event.target.files && event.target.files.length) {
+      const exc = event.target.files[0];
+      reader.readAsText(exc);
+      
+      reader.onload = () => {
+        this.file = reader.result.toString();
+
+        //Con una libreria alasql  libreria de javaScript  que te devuelve un Objeto excel
+
+        //Vamos a comvertir el string a objeto
+        //let obj = JSON.parse(this.file);
+        console.log(this.file);
+      };
+    }
+  }
 }
